29e7076744422f2c2f99600cbae32b0e47f47ea3
Maarten van Wersch <maartenvanwersch@hotmail.com>
2021-02-27 10:26:39 +0100

    index setup and firstname and lastname added

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,16 @@
-<template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        incentro-assignment
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+  .container.max-w-screen-sm.px-3
+    section.my-6
+      h1 Hi Incentro :)
+      p Check hier de opdracht van Maarten
+    section.my-6
+      form
+        .flex.flex-row.mb-3
+          input.flex-auto.block.w-full.border.border-gray-600.rounded-md.px-2.py-1.mx-2(type="text" placeholder="Voorletters")
+          input.flex-auto.block.w-full.border.border-gray-600.rounded-md.px-2.py-1.mx-2(type="text" placeholder="Tussenvoegsel")
+        .flex.flex-row.mb-3
+          input.flex-auto.block.w-full.border.border-gray-600.rounded-md.px-2.py-1.mx-2(type="text" placeholder="Achternaam")
+
 </template>
 
 <script>
@@ -40,39 +26,8 @@ export default {}
 .container {
   margin: 0 auto;
   min-height: 100vh;
-  display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
